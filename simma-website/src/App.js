56c85214c6bb3e89abcfafa8277add0acc5d711a
Giona Fossati <fossatigiona@gmail.com>
2021-01-03 01:00:07 +0100

    Navbar attempt

--- simma-website/src/App.js
+++ simma-website/src/App.js
@@ -3,8 +3,31 @@ import ReactDOM from "react-dom";
 import { Button } from "reakit/Button";
 import './App.css';
 
+const NavBar = () => (
+  <nav aria-label="Simma Fine Art Website" >
+    <ul role="menubar" className='navbar'>
+      <li role="none">
+        <a href="" className='navbar__item' role="menuitem" className='navbar__logo'>
+          Simma
+        </a>
+        </li>
+      <li role="none">
+          <a href="" className='navbar__item navbar_item_active' role="menuitem">Home</a>
+        </li>
+      <li role="none" >
+        <a href="" className='navbar__item' role="menuitem">About</a>
+        </li>
+      <li role="none">
+          <a href="" className='navbar__item' role="menuitem">Works</a> 
+        </li>
+    </ul>
+             
+  </nav>
+);
 function App() {
-  return <Button>Button</Button>;
-}
+  return (
+    <NavBar />
+  )
+  }
 
 export default App;
