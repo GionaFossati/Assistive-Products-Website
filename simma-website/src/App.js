2e2bed4b6645fa6b64414984a98f7939f6d1c7a4
Giona Fossati <fossatigiona@gmail.com>
2021-01-03 15:15:18 +0100

    Hero done

--- simma-website/src/App.js
+++ simma-website/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "reakit/Button";
 import Logo from './assets/logo.svg';
+import HeroImage from './assets/hero.png';
+import HeroBackImage from './assets/heroback.svg';
+import ImgDecoration1 from './assets/decoration.svg';
 import './App.css';
 import './typography.css';
 
@@ -9,7 +12,7 @@ const NavBar = () => (
   <nav aria-label="Simma Fine Art Website" >
     <ul role="menubar" className='navbar'>
       <li role="none">
-        <a href="" className='navbar__item' role="menuitem" className='navbar__logo' role="">
+        <a href="" className='navbar__item' className='navbar__logo' role="Simma Art Logo">
         <img src={Logo} alt="Simma Logo" />
         </a>
         </li>
@@ -28,15 +31,27 @@ const NavBar = () => (
 );
 
 
-const Hero = (props) => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-  
+const Hero = () => {
   return(
     <div className="hero">
       <h1 className="hero__title">Simma</h1>
-      <p> Toronto-based fine artist</p>
+      <p className="hero__subtitle"> Toronto-based fine artist</p>
       <Button className="button__primary">My Works</Button>
+      <img src={HeroImage} role="presentation" className="hero__image"></img>
+      <img src={HeroBackImage} role="presentation" className="hero__backimage"></img>
+      <img src={ImgDecoration1} role="presentation" className="hero__decoration"></img>
+
+    </div>
+  )
+}
+
+const Services = () => {
+  
+  
+  return(
+    <div className="services">
+      <h2 className="services__title">What do I do?</h2>
+
     </div>
   )
 }
@@ -46,6 +61,7 @@ function App() {
     <div className="root"> 
       <NavBar />
       <Hero />  
+      <Services />
       </div>
     
   )
