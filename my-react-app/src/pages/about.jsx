0498489cb95886367eded8ce4812e15f2ba4aee5
Bea <beatrice.trinidad@gmail.com>
2021-01-04 15:16:50 +0100

    Successful Flexbox grid on About page
    Using this: https://github.com/roylee0704/react-flexbox-grid

--- my-react-app/src/pages/about.jsx
+++ my-react-app/src/pages/about.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+<<<<<<< Updated upstream
 import NavBar from '../components/navbar';
 
 /* We simply can use an array and loop and print each user */
 const AboutPage = () => {
   return (
     <NavBar />
+=======
+import { Grid, Row, Col } from 'react-flexbox-grid';
+/* We simply can use an array and loop and print each user */
+const AboutPage = () => {
+  return (
+    <div>
+    <h1>Two columns, gap in the middle</h1>
+    <Grid>
+      <Row>
+        <Col xs={5} style={{ border: "1px solid teal" }}>
+          <p>First column</p>
+        </Col>
+        <Col
+          xs={5}
+          xsOffset={2}
+          style={{ border: "1px solid palevioletred" }}
+        >
+          <p>Second column</p>
+        </Col>
+      </Row>
+    </Grid>
+  </div>
+
+>>>>>>> Stashed changes
   );
 };
 
