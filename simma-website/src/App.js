161f738d8dd4d22592f469456a76be203e3bd9f4
Giona Fossati <fossatigiona@gmail.com>
2021-01-09 22:58:20 +0100

    Erased previous react-app

--- simma-website/src/App.js
+++ simma-website/src/App.js
@@ -1,83 +1,41 @@
-import React from "react";
-import { Button } from "reakit/Button";
-import { Separator } from "reakit/Separator";
-import Logo from './assets/logo.svg';
-import HeroImage from './assets/hero.png';
-import HeroBackImage from './assets/heroback.svg';
-import ImgDecoration1 from './assets/decoration.svg';
-import ServicesImage from './assets/servicesimage.png';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import UsersPage from "./pages/users";
+import WorksPage from "./pages/works";
+import ProductPage from "./pages/productpage";
+import AboutPage from "./pages/about";
+import Home from "./pages/home";
 import './typography.css';
 
-const NavBar = () => (
-  <nav aria-label="Simma Fine Art Website" >
-    <ul className='navbar'>
-      <li>
-        <a href="" className='navbar__item' className='navbar__logo' aria-label="Simma Logo">
-        <img src={Logo} role="presentation"/>
-        </a>
-        </li>
-        <Separator orientation="vertical" />;
-      <li>
-          <a href="" className='navbar__item navbar_item_active'>Home</a>
-        </li>
-      <li>
-        <a href="" className='navbar__item'>About</a>
-        </li>
-      <li>
-          <a href="" className='navbar__item'>Works</a> 
-        </li>
-    </ul>   
-  </nav>
-);
+//Import all needed Component for this tutorial
+import MainPage from "./pages/home"; ///< index.jsx will be automatically imported 
+//And render that route with the MainPage component for the root path /
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+//Pages
 
 
-const Hero = () => {
-  return(
-    <div className="hero">
-      <h1 className="hero__title">Simma</h1>
-      <p className="hero__subtitle"> Toronto-based fine artist</p>
-      <Button className="button__primary">My Works</Button>
-      <img src={HeroImage} role="presentation" className="hero__image"></img>
-      <img src={HeroBackImage} role="presentation" className="hero__backimage"></img>
-      <img src={ImgDecoration1} role="presentation" className="hero__decoration"></img>
-
-    </div>
-  )
-}
-
-const Services = () => {
-  
-  
-  return(
-    <div className="services">
-      <h2 className="services__title">What do I do?</h2>
-      <ul className='services__list'>
-      <li className="services__item">
-        <h3>Paintings</h3>
-        <p> I let my creativity flow to produce fine art paintings.</p>
-      </li>
-      <li>
-        <h3>Personalized Gifts</h3>
-        <p> I can provide you with unique high quality gifts for every occasion.</p>
-      </li>
-      </ul>
-      <img src={ServicesImage} role="presentation" className="services__image"></img>
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <div className="root"> 
-      <NavBar />
-      <div role="main">
-      <Hero />  
-      <Services />
-      </div>
-      </div>
-    
-  )
+class App extends Component {
+  render() {
+    return (
+      <Router>
+       {/*All our Routes goes here!*/}
+       <Route exact path="/" component={Home} />
+       <Route exact path="/users" component={UsersPage} />
+       <Route exact path="/works" component={WorksPage} />
+       <Route exact path="/about" component={AboutPage} />
+       <Route exact path="/product" component={ProductPage} />
+      </Router>
+    );
   }
+}
 
 export default App;
