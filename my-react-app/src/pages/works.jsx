925a916f3930cbcc6d7ade3e70eb499cc8024bd6
Giona Fossati <fossatigiona@gmail.com>
2021-01-09 16:29:21 +0100

    🔎 Search Bar in WorksPage!!

--- my-react-app/src/pages/works.jsx
+++ my-react-app/src/pages/works.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { data } from '../assets/productsJS.js'
 import "./works.css";
@@ -6,6 +6,7 @@ import "./works.css";
 import Product from '../components/productPreview';
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';
+import SearchBar from '../components/searchBar';
 
 
 import { Grid, Row, Col } from 'react-flexbox-grid';
@@ -57,17 +58,66 @@ const ProductsList = () => {
   )
 }
 
+const ProductsListUpdated = ({productlist=[]}) => {
+  return (
+    <Grid fluid className="products__grid">
+           <Row id="products__row">
+        { 
+          productlist.map( (singleProduct) => {
+           return (
+                <Product product={singleProduct}/>
+           )
+          })
+        }
+        </Row>
+      </Grid>
+  )
+}
+
 
 function WorkPage() {
+  const [input, setInput] = useState('');
+  const [productsListDefault, setProductsListDefault] = useState();
+  const [productsList, setProductsList] = useState();
+
+  const fetchData = async () => {
+    return(
+        data => {
+          setProductsList(data) 
+          setProductsListDefault(data)
+        }
+      )
+    }
+
+  const updateInput = async (input) => {
+     const filtered = productsListDefault.filter(singleProduct => {
+      return singleProduct.name.toLowerCase().includes(input.toLowerCase())
+     })
+     setInput(input);
+     setProductsList(filtered);
+  }
+
+  useEffect( () => {setProductsList(data) 
+    setProductsListDefault(data)},[]);
+
   return (
   <div>
       <NavBar />
       <main id="main">
+          
           <h1 id="page__title">My Works</h1>
           <h2 id="paintings__title">Collections</h2>
           <CollectionsList />
           <h2 id="paintings__title">Paintings</h2>
-          <ProductsList />
+          <Row className="presentation__center">
+              <Col xs={12} md={12} lg={12}>
+                  <SearchBar
+                    input={input} 
+                    onChange={updateInput}
+                  />
+            </Col>
+          </Row>
+          <ProductsListUpdated productlist={productsList}/>
         </main>
       <Footer />    
   </div>
