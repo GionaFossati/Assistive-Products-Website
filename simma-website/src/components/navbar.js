2f4a2184c3a76378276443f84a8aa6a71de6b583
Giona Fossati <fossatigiona@gmail.com>
2021-01-10 12:39:31 +0100

    🗺 NavBar now responsive

--- simma-website/src/components/navbar.js
+++ simma-website/src/components/navbar.js
@@ -6,15 +6,47 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const NavBar = () => (
   <header>
+    <nav aria-label="Simma Fine Art Website" className='navbar presentation__verticalcenter'>
     <a class="skip-to-content-link shadow__low" href='#main'>Skip to content</a>
-    <nav aria-label="Simma Fine Art Website" /* className="shadow__low" */>
+      <Grid fluid>
+        <ul>
+        <Row>
+          <Col xs={0} md={3} lg={9} id="logocol">
+              <li>
+              <a href="/" className='navbar__item navbar__logo' aria-label="Simma Logo - Go to Home Page">
+              <img src={Logo} alt=""/>
+              </a>
+              </li>
+            </Col>
+          <Col xs={4} md={3} lg={1}>
+              <li>
+                <a href="/" className='navbar__item'>Home</a>
+              </li>
+            </Col>
+          <Col xs={4} md={3} lg={1}>
+              <li>
+              <a href="/about" className='navbar__item'>About</a>
+              </li>
+            </Col>
+          <Col xs={4} md={3} lg={1}>
+              <li>
+                <a href="/works" className='navbar__item'>Works</a> 
+              </li>
+            </Col>
+        </Row>
+        </ul>
+      </Grid>
+    </nav>
+    {/* 
+    <a class="skip-to-content-link shadow__low" href='#main'>Skip to content</a>
+    <nav aria-label="Simma Fine Art Website">
       <ul className='navbar'>
         <li>
           <a href="/" className='navbar__item navbar__logo' aria-label="Simma Logo">
           <img src={Logo} alt=""/>
           </a>
           </li>
-          <Separator orientation="vertical" />
+          
         <li>
             <a href="/" className='navbar__item'>Home</a>
           </li>
@@ -25,7 +57,7 @@ const NavBar = () => (
             <a href="/works" className='navbar__item'>Works</a> 
           </li>
       </ul>   
-    </nav>
+    </nav> */}
   </header>
 );
 
