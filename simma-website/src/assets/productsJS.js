eae649e9e9fa7e38dd86e43056dc6550bb8e0391
Bea <beatrice.trinidad@gmail.com>
2021-01-11 01:52:05 +0100

    Finished all descriptions of Products

--- simma-website/src/assets/productsJS.js
+++ simma-website/src/assets/productsJS.js
@@ -1,148 +1,379 @@
 export const data = [
         {
             "id": 0,
-            "name": "Doll",
-            "material": "Oil on canvas",
-            "shortDesc": "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
-            "description": "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
-            "picture": require("../assets/homeimage1.png"),
+            "name": "Bleak",
+            "material": "Acrylic Paint on Canvas",
+            "shortDesc": "Original, Industrial Inspired Painting",
+            "description": "This painting is a newly released original from Simma's highly sought after 1940s inspired collection.",
+            "picture": require("../assets/1940s01.png"),
             "dimension1": {
-                "dimension": "38x12cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "121x334cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
             },
             "dimension2": {
-                "dimension": "123x122cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "224x668cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
             },
             "dimension3": {
-                "dimension": "533x121cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "500x1380cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
             }
         },
         {
             "id": 1,
-            "name": "Industrial Landscapes",
-            "material": "Oil on canvas",
-            "shortDesc": "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
-            "description": "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
-            "picture": require("../assets/homeimage2.png"),
+            "name": "A Day in the Forest",
+            "material": "Acrylic Paint, Mixed Media",
+            "shortDesc": "Original, Impressionist Painting",
+            "description": "This painting is a newly released original from Simma's highly sought after 1940s inspired collection.",
+            "picture": require("../assets/1940s02.png"),
             "dimension1": {
                 "dimension": "334x121cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
             },
             "dimension2": {
-                "dimension": "134x12cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "668x224cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
             },
             "dimension3": {
-                "dimension": "5333x121cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "1380x500cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
             }
         },
         {
             "id": 2,
-            "name": "Vintage Dresses",
-            "material": "Oil on canvas",
-            "shortDesc": "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
-            "description": "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
-            "picture": require("../assets/homeimage3.png"),
+            "name": "Monday",
+            "material": "Acrylic Paint on Canvas",
+            "shortDesc": "Original, Industrial Inspired Painting",
+            "description": "An impressionist depiction of a 1940s urban environment in the United States.",
+            "picture": require("../assets/1940s03.png"),
             "dimension1": {
-                "dimension": "3332x1121cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "334x121cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
             },
             "dimension2": {
-                "dimension": "1344x122cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "668x224cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
             },
             "dimension3": {
-                "dimension": "5433x1241cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "1380x500cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
             }
         },
         {
             "id": 3,
-            "name": "Vintage Dresses",
-            "material": "Oil on canvas",
-            "shortDesc": "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
-            "description": "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
-            "picture": require("../assets/homeimage3.png"),
+            "name": "Chilly Day in London",
+            "material": "Acrylic Paint, Mixed Media, Canvas",
+            "shortDesc": "Original, Industrial Inspired Painting",
+            "description": "A one-of-a-kind, impressionist, fine arts painting inspired by the industrial 1940s environment of London.",
+            "picture": require("../assets/1940s04.png"),
             "dimension1": {
-                "dimension": "3332x1121cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "121x334cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
             },
             "dimension2": {
-                "dimension": "1344x122cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "224x668cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
             },
             "dimension3": {
-                "dimension": "5433x1241cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "500x1380cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
             }
         },
         {
             "id": 4,
-            "name": "Vintage Dresses",
-            "material": "Oil on canvas",
-            "shortDesc": "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
-            "description": "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam.",
-            "picture": require("../assets/homeimage4.png"),
+            "name": "La Femme",
+            "material": "Oil on Canvas",
+            "shortDesc": "Doll Portrait Painting",
+            "description": "A rustic, one-of-a-kind, mesmerizing portrait of a woman in doll form.",
+            "picture": require("../assets/Dolls01.png"),
             "dimension1": {
-                "dimension": "3332x1121cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "100x100cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
             },
             "dimension2": {
-                "dimension": "1344x122cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "200x200m",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
             },
             "dimension3": {
-                "dimension": "5433x1241cm",
-                "bw": "€1",
-                "bw_frame": "€2",
-                "color": "€3",
-                "color_frame": "€4"
+                "dimension": "300x300cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 5,
+            "name": "Petite Fille",
+            "material": "Oil on Canvas",
+            "shortDesc": "Doll Portrait Painting",
+            "description": "A rustic, one-of-a-kind, mesmerizing portrait of a young girl in doll form.",
+            "picture": require("../assets/Dolls02.png"),
+            "dimension1": {
+                "dimension": "100x100cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "200x200cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "300x300cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 6,
+            "name": "Spring Time",
+            "material": "Acrylic Paint on Canvas ",
+            "shortDesc": "Contemporary Dress Painting",
+            "description": "Beautiful, vibrant, and wonderful textured painting of a dress made for the springtime.",
+            "picture": require("../assets/Dresses01.png"),
+            "dimension1": {
+                "dimension": "121x334cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "224x668cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "500x1380cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 7,
+            "name": "Bedroom Decor",
+            "material": "Acrylic Paint on Mixed Media",
+            "shortDesc": "Contemporary Dress Painting",
+            "description": "A rustic, contemporary art piece as part of the Dresses collection with an expression of freedom and sophistication.",
+            "picture": require("../assets/Dresses02.png"),
+            "dimension1": {
+                "dimension": "121x334cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "224x668cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "500x1380cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 8,
+            "name": "Elegance",
+            "material": "Acrylic Paint on Mixed Media",
+            "shortDesc": "Contemporary Dress Painting",
+            "description": "Contemporary and fine arts painting of a beautiful, elegant, classic dress with attention to detail that will liven up any room.",
+            "picture": require("../assets/Dresses03.png"),
+            "dimension1": {
+                "dimension": "121x334cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "224x668cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "500x1380cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 9,
+            "name": "Joyful Jerusalem",
+            "material": "Watercolor, Mixed Media",
+            "shortDesc": "Original, Landscape Fine Art Painting",
+            "description": "Stunning painting of an overview of Jerusalem with a colorful pallete with shades of  purple, green, yellow, orange and pink. This view of Jerusalemn posesses an elegant, casual sophistication and captures the eye of the viewer with its bold yet gentle design.",
+            "picture": require("../assets/Jerusalem01.png"),
+            "dimension1": {
+                "dimension": "100x100cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "200x200m",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "300x300cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 10,
+            "name": "Jerusalem at Twilight",
+            "material": "Watercolor, Mixed Media",
+            "shortDesc": "Original, Landscape Fine Art Painting",
+            "description": "The beauty of Jerusalem is present and is the main focus among the softened colors in the background. Beautiful, vibrant, and sure to be the focal point of any room decor.",
+            "picture": require("../assets/Jerusalem02.png"),
+            "dimension1": {
+                "dimension": "100x100cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "200x200m",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "300x300cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 11,
+            "name": "Sunrise",
+            "material": "Watercolor, Mixed Media",
+            "shortDesc": "Original, Landscape Fine Art Painting",
+            "description": "Stunning painting of a beautiful sunset over Jerusalem with vibrant colors of orange, yellow, red, and some black.",
+            "picture": require("../assets/Jerusalem03.png"),
+            "dimension1": {
+                "dimension": "100x100cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "200x200m",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "300x300cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
+            }
+        },
+        {
+            "id": 12,
+            "name": "Life in Jerusalem",
+            "material": "Mixed Media, Acrylic Paint",
+            "shortDesc": "Original, Landscape Fine Art Painting",
+            "description": "Modern and textured painting of a bright day in Jerusalem. The painting gives a lively feeling as the viewer can see people walking around the grand city of Jerusalem, with the light shining boldly over the city.",
+            "picture": require("../assets/Jerusalem04.png"),
+            "dimension1": {
+                "dimension": "121x334cm",
+                "bw": "€210",
+                "bw_frame": "€420",
+                "color": "€350",
+                "color_frame": "€560"
+            },
+            "dimension2": {
+                "dimension": "224x668cm",
+                "bw": "€360",
+                "bw_frame": "€550",
+                "color": "€450",
+                "color_frame": "€690"
+            },
+            "dimension3": {
+                "dimension": "500x1380cm",
+                "bw": "€490",
+                "bw_frame": "€650",
+                "color": "€590",
+                "color_frame": "€780"
             }
         }
-        
     ]
